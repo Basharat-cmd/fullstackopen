60ef1a4a1f6893c1c0d70000dd050d989d2dfd4d
Basharat-cmd <openchat50@gmail.com>
2026-01-05 15:00:51 +0500

    ex:2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -20,11 +20,22 @@ const Content = ({ parts }) => {
   )
 }
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <p>
+      <strong>Total of {total} exercises</strong>
+    </p>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -36,7 +47,8 @@ const App = () => {
     parts: [
       { name: 'Fundamentals of React', exercises: 10, id: 1 },
       { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 }
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 11, id: 4 }
     ]
   }
 
